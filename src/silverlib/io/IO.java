188a99e2ba96cb36d65304e9d05f3409644ac7a9
Jake Trimble <jatnerd@gmail.com>
2017-09-14 13:08:53 -0700

    $ io.IO.readFile:readLines

--- src/silverlib/io/IO.java
+++ src/silverlib/io/IO.java
@@ -6,48 +6,42 @@ import silverlib.log.*;
 
 public class IO {
   public static String getPath(String fnm,File script) throws IOException{
-    String[] command = {script.getAbsolutePath()};         
-    Process process = Runtime.getRuntime().exec(command);                    
-    BufferedReader reader = new BufferedReader(new InputStreamReader(        
-        process.getInputStream()));                                          
-    String s;                                                                
-    while ((s = reader.readLine()) != null) {                                
-      return s+"/"+fnm;                         
+    String[] command = {script.getAbsolutePath()};
+    Process process = Runtime.getRuntime().exec(command);
+    BufferedReader reader = new BufferedReader(new InputStreamReader(
+        process.getInputStream()));
+    String s;
+    while ((s = reader.readLine()) != null) {
+      return s+"/"+fnm;
     }
-    return null;  
-  }
-  
-  public static String readFile(File f){
-    try{
-			FileInputStream in = new FileInputStream(f);
-			String contents = "";
-			
-			while(in.available() > 0){
-				contents += (char) in.read();
-			}
-			in.close();
-			
-			return contents;
-		}catch(IOException e){
-			Err.log("Couldn't read File");
-      return null;
-		}
+    return null;
   }
 
-  public static String[] readLines(File f){
-    try{
+  public static String readFile(File f) throws IOException{
+
 			FileInputStream in = new FileInputStream(f);
 			String contents = "";
-			
+
 			while(in.available() > 0){
 				contents += (char) in.read();
 			}
 			in.close();
-			
+
+			return contents;
+
+  }
+
+  public static String[] readLines(File f) throws IOException{
+
+			FileInputStream in = new FileInputStream(f);
+			String contents = "";
+
+			while(in.available() > 0){
+				contents += (char) in.read();
+			}
+			in.close();
+
 			return contents.split("\n");
-		}catch(IOException e){
-			Err.log("Couldn't read File");
-      return null;
-		}
+
   }
 }
