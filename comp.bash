ce1f13c46dbee92c0ca6e36773ab404f93a238ca
SiLordOfLight <jatnerd@gmail.com>
2017-09-19 19:05:55 -0700

    Update run script

--- comp.bash
+++ comp.bash
@@ -4,15 +4,4 @@ cd ~/Stuff/Creations/Programming/silverlib
 
 ant
 
-if [ ! -f silverlib.jar ]; then
-  echo "Failiure!!!"
-  exit
-fi
-
-cd lib
-
-rm silverlib.jar
-
-cd ..
-
-mv silverlib.jar lib/silverlib.jar
+cp -r doc/* ../silverlibDoc/
