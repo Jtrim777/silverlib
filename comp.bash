503cfcb82d4d166ea061d47b4ba65eb10562f427
SiLordOfLight <jatnerd@gmail.com>
2017-09-26 20:48:53 -0700

    Build

--- comp.bash
+++ comp.bash
@@ -1,42 +1,87 @@
 #!/bin/bash
 DATE=`date '+%Y-%m-%d %H:%M:%S'`
+loc="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ant -S
+cd $loc
 
-cp -r doc/* ../silverlibDoc/
+function main {
+  #Build
+  echo "Building Library..."
+  ant -S
+  
+  #Move javadocs to silverlibDoc
+  echo "Migrating JavaDocs..."
+  cp -r doc/* ../silverlibDoc/
+  
+  #Copy jar to dist
+  echo "Moving Jar..."
+  cp lib/silverlib.jar dist/
+  
+  #Zip dist and move it to silverlibDoc
+  echo "Creating distribution file..."
+  zip -r ../silverlibDoc/Silverlib.zip dist
+  
+  #Remove doc&build
+  echo "Cleaning directories..."
+  ant -S clean
+}
 
-cp lib/silverlib.jar dist/
+function buildOnly {
+  #Build
+  echo "Building Library..."
+  ant -S
+  
+  #Remove doc,build,and dist
+  echo "Cleaning directories..."
+  ant -S clean
+}
 
-zip -r ../silverlibDoc/Silverlib.zip dist
-
-ant -S clean
-
-if [ $# == 0 ]; then
+function pushDoc {
   cd ../silverlibDoc
-
+  
+  echo "Pushing JavaDocs..."
   git add .
   git commit -m "$DATE UPDATE"
   git push -u origin master
+}
+
+function pushIt {
+  echo "Pushing source with commit message $1..."
+  git add .
+  git commit -m $1
+  git push -u origin master
+}
+
+function zipIt {
+  #Zip dist and move it to silverlibDoc
+  echo "Creating distribution file..."
+  zip -r ../silverlibDoc/Silverlib.zip dist
+}
+
+function versionUpdate {
+  echo "Modifying version number and info..."
+  echo -e "$1;$2" > "dist/info.txt"
+}
+
+
+if [[ $# == 0 ]]; then
+  main
+  pushDoc
+elif [[ $1 == "-b" ]]; then
+  buildOnly
 elif [[ $1 == "-d" ]]; then
-  cd ../silverlibDoc
-
-  git add .
-  git commit -m "$DATE UPDATE"
-  git push -u origin master
+  pushDoc
 elif [[ $1 == "-p" ]]; then
-  git add .
-
-  if [[ $# == 2 ]]; then
-    git commit -m "CHANGES $DATE"
-  else
-    git commit -m $3
-  fi
-
-  git push -u origin master
-
-  cd ../silverlibDoc
-
-  git add .
-  git commit -m "$DATE UPDATE"
-  git push -u origin master
+  pushIt $2
+elif [[ $1 == "-z" ]]; then
+  zipIt
+elif [[ $1 == "-v" ]]; then
+  versionUpdate "$2" "$3"
+elif [[ $1 == "-h" ]]; then
+  echo -e "No args: Build library, copy javadocs to silverlibDoc, create dowload zip,\nremove extra directories, push silverlibDoc"
+  echo -e "-b: Only build the library"
+  echo -e "-d: Push silverlibDoc"
+  echo -e "-p: Push the Library source"
+  echo -e "-z: Create a zipped dist"
+  echo -e "-v: Update the version, where  arg 1 is version number and arg 2 is version info"
 fi
--- dist/silverlib.bash
+++ dist/silverlib.bash
@@ -1,9 +1,21 @@
 #!/bin/bash
 THE_OS=`uname`
 loc="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+INSTALLED=false
 
-if [[ $THE_OS == 'Darwin' ]]; then
-  #Install
+cd $loc
+ILIST=$(cat info.txt)
+OIFS=$IFS
+IFS=';' read -r -a INFO <<< "$ILIST"
+IFS=$OIFS
+VERSION="${INFO[0]}"
+VERSION_INFO="${INFO[1]}"
+
+if [[ $loc -ef ~/Library/silverlib ]]; then
+  INSTALLED=true
+fi
+
+function install {
   cd ~/Library
 
   if [ -d "silverlib" ]; then
@@ -11,23 +23,64 @@ if [[ $THE_OS == 'Darwin' ]]; then
     cd silverlib
     rm -f silverlib.jar
     cd $loc
-    cp silverlib.jar ~/Library/silverlib/
+    cp -r ./* ~/Library/silverlib/
   else
     #Create
     mkdir "silverlib"
     cd $loc
-    cp silverlib.jar ~/Library/silverlib/
+    cp -r ./* ~/Library/silverlib/
 
     cd ~/
     if [ -f ".bash_profile" ]; then
       #Append
       echo -e "\nexport PATH=${PATH}:~/Library/silverlib/" >> .bash_profile
-      echo -e "\nexport CLASSPATH=${CLASSPATH}:~/Library/silverlib/silverlib.jar" >> .bash_profile
+      echo -e "export CLASSPATH=${CLASSPATH}:~/Library/silverlib/silverlib.jar" >> .bash_profile
+      echo -e "alias silverlib=~/Library/silverlib/silverlib.bash" >> .bash_profile
+      # echo -e "export SILVERLIB_VERSION='$VERSION'" >> .bash_profile
     else
       #Create and add
-      echo -e "\nexport PATH=${PATH}:~/Library/silverlib/" > .bash_profile
-      echo -e "\nexport CLASSPATH=${CLASSPATH}:~/Library/silverlib/silverlib.jar" >> .bash_profile
+      echo -e "export PATH=${PATH}:~/Library/silverlib/" > .bash_profile
+      echo -e "export CLASSPATH=${CLASSPATH}:~/Library/silverlib/silverlib.jar" >> .bash_profile
+      echo -e "alias silverlib=~/Library/silverlib/silverlib.bash" >> .bash_profile
+      # echo -e "export SILVERLIB_VERSION='$VERSION'" >> .bash_profile
     fi
+  fi
+  
+  cd ~/Library/silverlib
+  git init
+  git remote add -f origin https://gitlab.com/SiLordOfLight/silverlib.git
+  git config core.sparseCheckout true
+  echo "dist/silverlib.jar" >> .git/info/sparse-checkout
+  echo "dist/info.txt" >> .git/info/sparse-checkout
+}
+
+function update {
+  cd ~/Library/silverlib
+  git pull origin master
+}
+
+if [[ $THE_OS == 'Darwin' ]]; then
+  if [ $INSTALLED == false ] && [ $# == 0 ]; then
+    install
+  elif [ $INSTALLED == true ] && [ $# == 0 ]; then
+    update
+  elif [[ $1 == '-i' ]]; then
+    install
+  elif [[ $1 == '-u' ]]; then
+    update
+  elif [[ $1 == '-v' ]]; then
+    echo -e "$VERSION"
+  elif [[ $1 == '-V' ]]; then
+    echo -e "Silverlib Java Library\n Copyright 2017 Jake Trimble" 
+    echo -e "Version $VERSION"
+    echo -e "$VERSION_INFO"
+  elif [[ $1 == '-h' ]]; then
+    echo "-i to install library"
+    echo "-u to update library"
+    echo "-v to get library version"
+    echo "-V to get version info on this library"
+    echo ""
+    echo -e "Go to silordoflight.github.io/silverlib for Java Documentation on the classes\nin this library"
   fi
 else
   echo "We don't serve your kind"
