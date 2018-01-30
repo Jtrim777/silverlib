d499760e55583fa7ac05f73e22c8d6ad6f3b4fdf
Jake Trimble <jatnerd@gmail.com>
2018-01-30 14:03:58 -0800

    v1.8.3-b002

--- src/silverlib/math/Graph.java
+++ src/silverlib/math/Graph.java
@@ -4,5 +4,17 @@ import java.io.File;
 import java.awt.Color;
 
 public class Graph extends Image{
+  private Function fx;
+  private Point origin;
+  private int xScale;
+  private int yScale;
 
+  public Graph(Function f, Point o, int w, int h){
+    super(w,h);
+
+    fxTem = f.translateY(o.y);
+    fx = fxTem.reflectX();
+
+    origin = o;
+  }
 }
---
+++ src/silverlib/math/LinearFunc.java
@@ -0,0 +1,10 @@
+public class LinearFunc extends Function{
+
+  public LinearFunc(double b, double h, double c){
+    super(b,h,c);
+  }
+
+  public LinearFunc(String f){
+    
+  }
+}
